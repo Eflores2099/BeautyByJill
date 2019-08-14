cca0102f38c6e0e4ae28f508c71e47760e960c73
Erik Flores <erik.flores@colorado.edu>
2019-08-14 14:31:00 -0600

    postCard

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -4,7 +4,12 @@ const postCard = () => {
     return(
         <div className ='postFeed'>
             <article className = 'postCard' style="backgroundImage: url">
+                <a className="postLink" href="">
+                    <div className =" postContent">
+                        <h2 className="postTitle"></h2>
 
+                    </div>
+                </a>
             </article>
 
         </div>
